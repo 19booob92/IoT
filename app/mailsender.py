7d12cd356dba3cfe7a1d5731b0c0288c4f1303ce
Mateusz <19mateusz92@gmail.com>
2017-04-03 20:59:09 +0200

    Fixed mail sendig

--- app/mailsender.py
+++ app/mailsender.py
@@ -1,20 +1,27 @@
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-USER = 'webtestkonto2015@gmail.com'
-PASS = 'samolot123'
+import config
+
+USER = config.email
+PASS = config.password
+TO = config.recipient
+
 SMTP_SERVER = 'smtp.gmail.com'
 SMTP_PORT = 587
 
-def send_email(recipient, subject, text):
+def send_email(msg):
+    content = MIMEMultipart('alternative')
     smtpserver = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
     smtpserver.ehlo()
     smtpserver.starttls()
     smtpserver.ehlo
     smtpserver.login(USER, PASS)
-    header = 'Do:' + recipient + '\n' + 'Od: ' + USER
-    header = header + '\n' + 'Temat:' + subject + '\n'
-    msg = header + '\n' + text + ' \n\n'
-    smtpserver.sendmail(USER, recipient, msg)
-    smtpserver.close()
 
-send_email('19mateusz92@gmail.com', 'test', 'test')
+    content['Subject'] = 'Mieszkanie PasPI'
+    text = MIMEText(msg, 'plain')
+    content.attach(text)
+
+    smtpserver.sendmail(USER, TO, content.as_string())
+    smtpserver.close()
