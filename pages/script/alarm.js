7d471a3a4501175ca399c618ff59f795969757aa
Mateusz <19mateusz92@gmail.com>
2017-04-04 23:54:50 +0200

    Fixed js lib import

--- pages/script/alarm.js
+++ pages/script/alarm.js
@@ -1,19 +1,4 @@
-var baseUrl = 'http://192.168.8.100:8081/'
-
-setUpPage = function() {
-    var secondsToDeactivate = document.getElementsByName("secondsToDeactivate")[0];
-    var sendMailDelay = document.getElementsByName("sendMailDelay")[0];
-
-    var http = new XMLHttpRequest();
- 	http.open( "GET", baseUrl + 'loadAlarmConfig', true );
-	http.send();
-	http.onload = function() {
-        var alarmData = JSON.parse(http.responseText);
-
-        secondsToDeactivate.value = alarmData.secondsToDeactivate;
-        sendMailDelay.value = alarmData.sendMailDelay;
-    };
-};
+var baseUrl = 'http://192.168.8.100:8081/';
 
 function setUpAlarmElements() {
     var btn = document.getElementById('alarmToggler');
