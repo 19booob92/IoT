104decd29e9676543eaf4b8ddfa670efcf5ac270
Mateusz <19mateusz92@gmail.com>
2017-04-08 16:46:21 +0200

    Added diode blinking on alarm state change Fixed bug

--- app/rpi/alarm.py
+++ app/rpi/alarm.py
@@ -1,10 +1,21 @@
+import time
+from color import *
 
 class Alarm():
+
     def setUpAlarm(self):
+        self.scanPort()
+        self.blinkDiode(repts = 5, color = RED)
+
+    def scanPort(self):
         pass
 
     def disableAlarm(self):
-        pass
+        self.blinkDiode(repts=2, color=BLUE)
+
+    def blinkDiode(self, repts, color):
+        for i in range(repts):
+            pass
 
     def setTimeForDisable(self):
         pass
